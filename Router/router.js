69eb42d683e07a9a55e680b72c5b7b462873ad36
Menchi tayeb <tayebmenchi@outlook.fr>
2025-02-11 18:18:11 +0100

    Ajout système routage

---
+++ Router/router.js
@@ -0,0 +1,52 @@
+import Route from "./Route.js";
+import { allRoutes, websiteName } from "./allRoutes.js";
+
+// Création d'une route pour la page 404 (page introuvable)
+const route404 = new Route("404", "Page introuvable", "/pages/404.html");
+
+// Fonction pour récupérer la route correspondant à une URL donnée
+const getRouteByUrl = (url) => {
+  let currentRoute = null;
+  allRoutes.forEach((element) => {
+    if (element.url == url) {
+      currentRoute = element;
+    }
+  });
+  if (currentRoute != null) {
+    return currentRoute;
+  } else {
+    return route404;
+  }
+};
+
+// Fonction pour charger le contenu de la page
+const LoadContentPage = async () => {
+  const path = window.location.pathname;
+  const actualRoute = getRouteByUrl(path);
+  const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
+
+  if (actualRoute.pathJS != "") {
+    var scriptTag = document.createElement("script");
+    scriptTag.setAttribute("type", "text/javascript");
+    scriptTag.setAttribute("src", actualRoute.pathJS);
+    document.querySelector("body").appendChild(scriptTag);
+  }
+
+  document.title = actualRoute.title + " - " + websiteName;
+};
+
+// Fonction pour gérer les événements de routage (clic sur les liens)
+const routeEvent = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  LoadContentPage();
+};
+
+// Gestion de l'événement de retour en arrière dans l'historique du navigateur
+window.onpopstate = LoadContentPage;
+// Assignation de la fonction routeEvent à la propriété route de la fenêtre
+window.route = routeEvent;
+// Chargement du contenu de la page au chargement initial
+LoadContentPage();
