4e258e749ce5e15efa50e99fc75ab696b61807f3
Menchi tayeb <tayebmenchi@outlook.fr>
2025-02-13 13:27:41 +0100

    Add image BigTitle

--- Router/router.js
+++ Router/router.js
@@ -7,11 +7,13 @@ const route404 = new Route("404", "Page introuvable", "/pages/404.html");
 // Fonction pour récupérer la route correspondant à une URL donnée
 const getRouteByUrl = (url) => {
   let currentRoute = null;
+  // Parcours de toutes les routes pour trouver la correspondance
   allRoutes.forEach((element) => {
     if (element.url == url) {
       currentRoute = element;
     }
   });
+  // Si aucune correspondance n'est trouvée, on retourne la route 404
   if (currentRoute != null) {
     return currentRoute;
   } else {
@@ -22,17 +24,25 @@ const getRouteByUrl = (url) => {
 // Fonction pour charger le contenu de la page
 const LoadContentPage = async () => {
   const path = window.location.pathname;
+  // Récupération de l'URL actuelle
   const actualRoute = getRouteByUrl(path);
+  // Récupération du contenu HTML de la route
   const html = await fetch(actualRoute.pathHtml).then((data) => data.text());
+  // Ajout du contenu HTML à l'élément avec l'ID "main-page"
   document.getElementById("main-page").innerHTML = html;
 
+  // Ajout du contenu JavaScript
   if (actualRoute.pathJS != "") {
+    // Création d'une balise script
     var scriptTag = document.createElement("script");
     scriptTag.setAttribute("type", "text/javascript");
     scriptTag.setAttribute("src", actualRoute.pathJS);
+
+    // Ajout de la balise script au corps du document
     document.querySelector("body").appendChild(scriptTag);
   }
 
+  // Changement du titre de la page
   document.title = actualRoute.title + " - " + websiteName;
 };
 
@@ -40,7 +50,9 @@ const LoadContentPage = async () => {
 const routeEvent = (event) => {
   event = event || window.event;
   event.preventDefault();
+  // Mise à jour de l'URL dans l'historique du navigateur
   window.history.pushState({}, "", event.target.href);
+  // Chargement du contenu de la nouvelle page
   LoadContentPage();
 };
 
