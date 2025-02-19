32be284db9bc39d12b5c536ab115e19448b9b69d
Menchi tayeb <tayebmenchi@outlook.fr>
2025-02-19 22:59:39 +0100

    add login and registration page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,10 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/Covoiturage", "Covoiturage", "/pages/Covoiturage.html"),
+    new Route("/covoiturage", "Covoiturage", "/pages/Covoiturage.html"),
+    new Route("/signin", "connexion", "/pages/auth/signin.html"),
+    new Route("/signup", "inscription", "/pages/auth/signup.html"),
+    new Route("/account", "monCompte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
